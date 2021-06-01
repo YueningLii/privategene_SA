7b802a68e66e499f135c16743c41762ede8d67c5
yuening li <yuening0303@gmail.com>
2021-06-01 15:05:13 +0100

    R code for mapping private genes
    Add R code for mapping the location. Generate a dataframe with a column showing whether the private genes are present or not.
    reference genome: 4311_3#9.velvet.gff

---
+++ private gene locations .R
@@ -0,0 +1,49 @@
+#packages 
+chooseCRANmirror()
+install.packages("BiocManager")
+BiocManager::install("ballgown")
+install.packages("grid")
+library(ballgown)
+library(circlize)
+library(gridBase)
+library(ComplexHeatmap)
+library(gridExtra)
+library("grid")
+
+#read reference gff
+ref<-gffReadGR("4311_3#9.velvet.gff",identifier = "gene_id")
+#read gene id data 
+geneid<-read.csv("geneid.csv")
+names(geneid)[2]<-"ID"
+
+
+####create data frame 
+ID=ref$ID
+gene_name=ref@elementMetadata$gene
+start=as.numeric(ref@ranges@start)
+end=as.numeric(ref@ranges@start + ref@ranges@width + 1)
+strand = ifelse(ref@strand == "+",1,0)
+
+ref.df<-data.frame(
+  "start" = start,
+  "end" = end,
+  "strand" = strand,
+  "gene_name" = gene_name,
+  "ID" = ID
+)
+
+#append the private gene list 
+#join by id 
+ref.df<-dplyr::left_join(ref.df,geneid,by="ID")
+not_listed<-dplyr::anti_join(geneid,ref.df,by="ID") # the list of genes which can not be found on this gff
+
+#data transformation,add a column called "private",if gene is present : "1",if not:"0"
+
+vec=ref.df$Gene
+n = length(vec)
+for(i in 1:n){if(is.na(vec[i])== TRUE){vec[i]= 0}else{vec[i]= 1}}
+length(which(vec == 0))
+ref.df$private=vec
+
+##sort the start position 
+ref.df$start<-sort(ref.df$start)
